8d7e7341eea3e1c8e5d7e456cc8a29f870246b10
Felipe Damasceno <felipeodamasceno@hotmail.com>
2026-01-12 10:09:54 -0300

    correção de teste

--- cypress/e2e/products/products.cy.ts
+++ cypress/e2e/products/products.cy.ts
@@ -4,7 +4,6 @@ describe('Inventory', () => {
     beforeEach(() => {
         cy.visit("https://www.saucedemo.com/");
         cy.login();
-        inventory.verificarUrl();
     });
 
     it('Adicionar item ao carrinho', () => {
