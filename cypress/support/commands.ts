2c5aee6235a28415a3ab18126d9dc01486dd59b5
Felipe Damasceno <felipeodamasceno@hotmail.com>
2026-01-12 10:26:39 -0300

    correções

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,5 +3,4 @@ Cypress.Commands.add('login', () => {
     cy.get("[data-test=username]").type(Cypress.env('username'));
     cy.get("[data-test=password]").type(Cypress.env('password'));
     cy.get("[data-test=login-button]").click();
-    cy.url().should("eq", "https://www.saucedemo.com/inventory.html")
 });
