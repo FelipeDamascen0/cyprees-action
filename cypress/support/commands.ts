6ace941a36b19c3712eb370124d566aa3b570a97
Felipe Damasceno <felipeodamasceno@hotmail.com>
2026-01-12 10:37:10 -0300

    usando usario e senha fixa

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,6 +1,6 @@
 Cypress.Commands.add('login', () => {
     cy.visit("https://www.saucedemo.com/");
-    cy.get("[data-test=username]").type(Cypress.env('username'));
-    cy.get("[data-test=password]").type(Cypress.env('password'));
+    cy.get("[data-test=username]").type('standard_user');
+    cy.get("[data-test=password]").type('secret_sauce');
     cy.get("[data-test=login-button]").click();
 });
